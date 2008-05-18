45a9bb9c38b58d8222c3496c32808b698aa1db12
bentorfs <bentorfs@c6d29e44-364d-0410-9d05-3f146b4bb521>
2008-05-18 15:27:15 +0000

    Configuration is een klasse waar we wa static members kunne insteken die parameters zijn voor de simulatie. Bv hoeveel timesteps voor een reservatie verdwijnt.

--- trunk/inlandshipping/inlandshipping/LockPosition.java
+++ trunk/inlandshipping/inlandshipping/LockPosition.java
@@ -1,9 +1,12 @@
 package inlandshipping;
 
 /**
- * The position a lock is in. NEXTLEVEL for the water level of the 'next' segment,
- * PREVIOUSLEVEL for the water level of the 'previous' segment
+ * The position a lock is in. 
+ * NEXTLEVEL for the water level of the 'next' segment,
+ * PREVLEVEL for the water level of the 'previous' segment,
+ * TONEXT for water level inbetween, underway to the level of the 'next' segment,
+ * TOPREV for water level inbetween, underway to the level of the 'previous' segment.
  */
 public enum LockPosition {
-	NEXTLEVEL, PREVIOUSLEVEL
+	NEXTLEVEL, PREVLEVEL, TONEXT, TOPREV
 }
---
+++ trunk/inlandshipping/inlandshipping/ResAgent.java
@@ -0,0 +1,31 @@
+package inlandshipping;
+
+import java.util.Iterator;
+import java.util.Vector;
+
+public class ResAgent {
+	/*
+	 * The reservations kept by this agent.
+	 */
+	private Vector<LockReservation> reservations = new Vector<LockReservation>();
+	
+	/**
+	 * Removes a reservation from the list of reservations.
+	 * @param reservation
+	 */
+	public void expire(LockReservation reservation) {
+		reservations.remove(reservation);
+	}
+	
+	/**
+	 * Decreases the TTL of all reservations of this agent by 1.
+	 *
+	 */
+	public void increaseTTL() {
+		Iterator<LockReservation> i = reservations.iterator();
+		while (i.hasNext()) {
+			i.next().decreaseTTL();
+		}
+	}
+
+}
